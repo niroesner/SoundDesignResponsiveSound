e5a872ae6b5976cfc3c600594efe9042c9dfee62
Jeko <52203544+JekoDev@users.noreply.github.com>
2020-06-15 16:12:27 +0200

    Update
    Bomben gehen wieder, Falldown Sound etc, Licht aus

---
+++ Assets/Scripts/BombScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombScript : MonoBehaviour
+{
+    public Vector3 Respawn;
+    public AudioSource Explosion;
+
+    public GameObject Player;
+    private GameObject Bomb; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Bomb = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!Player || !Bomb) return;
+
+        float distance = Vector3.Distance(Player.transform.position, Bomb.transform.position);
+
+        if (distance < 1.8f)
+        {
+            Explosion.Play();
+            distance = 2;
+
+
+
+            Player.GetComponent<CharacterController>().enabled = false;
+            
+            Player.transform.position = Respawn;
+            Player.GetComponent<CharacterController>().enabled = true;
+        }
+    }
+}
---
+++ Assets/Scripts/FallDown.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallDown : MonoBehaviour
+{
+    public AudioSource FallDownSound;
+    public GameObject respawn;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!respawn) return;
+        if (this.transform.position.y < -3f)
+        {
+            this.GetComponent<CharacterController>().enabled = false;
+            this.transform.position = respawn.transform.position;
+            this.GetComponent<CharacterController>().enabled = true;
+            FallDownSound.Play();
+        }
+    }
+}
